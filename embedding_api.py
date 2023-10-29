ae67001ebd41993f21b02f10fbd1e76c531077de
Griffin Li <griffin@griffinli.com>
2023-10-28 22:32:55 -0400

    Add health check

--- embedding_api.py
+++ embedding_api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from sentence_transformers import SentenceTransformer
 from fastapi.middleware.cors import CORSMiddleware
 
-model = SentenceTransformer('all-mpnet-base-v2')
+model = None
 
 app = FastAPI()
 
@@ -18,8 +18,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.on_event("startup")
+async def load_model():
+    global model
+    model = SentenceTransformer('all-mpnet-base-v2')
+
 @app.get("/embedding")
 def read_root(q: str):
 
     embedding = model.encode(q)
     return {"embedding": embedding.tolist()}
+
+@app.get("/health")
+def read_health():
+
+    return {"status": "healthy"}
